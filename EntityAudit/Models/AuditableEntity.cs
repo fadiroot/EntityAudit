0c2a6ab49671188a91b2c0180e9a39c4a3878dd7
fedi.romdhan <fadiromdhan2@gmail.com>
2025-05-29 08:22:42 +0100

    update models

--- EntityAudit/Models/AuditableEntity.cs
+++ EntityAudit/Models/AuditableEntity.cs
@@ -6,7 +6,7 @@ namespace EntityAudit.Models;
 public abstract class AuditableEntity : IAuditable
 {
     public DateTime CreatedAt { get; set; }
-    public string CreatedBy { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
     public DateTime? UpdatedAt { get; set; }
-    public string UpdatedBy { get; set; }
+    public string UpdatedBy { get; set; } = string.Empty;
 }
